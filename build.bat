8fec8d605e847e59034439038018ba50538b66be
MrRobin <mrrobinjohannesson@gmail.com>
2024-11-17 04:14:22 +0100

    fix: pyinstaller command issue

--- build.bat
+++ build.bat
@@ -13,7 +13,7 @@ REM Install PyInstaller if not already installed
 pip install pyinstaller
 
 REM Run PyInstaller to create the .exe file with the required dependencies
-pyinstaller --onefile --name shutdownx --add-data "requirements.txt;." --copy-metadata readchar main.py
+pyinstaller --onefile --name shutdownx --add-data "requirements.txt:." --copy-metadata readchar main.py
 
 REM Deactivate the virtual environment after building the exe
 deactivate
--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ pip install -r requirements.txt
 pip install pyinstaller
 
 # Run PyInstaller to create the .exe file with the required dependencies
-pyinstaller --onefile --name shutdownx --add-data "requirements.txt;." --copy-metadata readchar main.py
+pyinstaller --onefile --name shutdownx --add-data "requirements.txt:." --copy-metadata readchar main.py
 
 # Deactivate the virtual environment after building the exe
 deactivate
